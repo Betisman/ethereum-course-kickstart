711b1cf238ab128a210bc9034b854329ec9aeb25
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-14 23:29:13 +0200

    refactoring deploy script for compiled contract

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
   'select wage version shield surge fringe crater silk pass tip course plug',
@@ -11,14 +11,12 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log('Attempting to deploy from accounts', accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
-
-  console.log('end')
-  console.log('interface', interface)
+  
   console.log('Contract deployed to', result.options.address)
 };
 deploy();
