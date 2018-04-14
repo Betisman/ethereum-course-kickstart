954682556a16155017deaf78e3c74bf3255d359e
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-15 00:52:44 +0200

    preparing index page to list campaigns. provider adapted to be in the browser or in the server

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,17 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentPovider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // We are in the borwser and metamask is running
+  web3 = new Web3(window.web3.currentPovider);
+} else {
+  // We are on the server OR the user is no running metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/GN6SwkKdQHdRUH9fRO3Y'
+  );
+  web3 = new Web3(provider);
+}
+
 
 export default web3;
