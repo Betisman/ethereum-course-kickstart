5f5d8142b6bcf9bccd8ca64d04bb3496a4ba21b2
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-16 00:59:11 +0200

    grabbing sumnmary data from the campaign contract

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[3],
+    };
+  }
+  
   render() {
     return (
       <Layout>
