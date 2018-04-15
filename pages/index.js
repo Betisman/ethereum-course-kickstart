f9be886ca01214802323c2d8784af09e59e86c71
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-15 22:21:15 +0200

    card and button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
@@ -7,8 +8,30 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>
+    return (
+      <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
+        {this.renderCampaigns()}
+        <Button
+          content="Create Campaign"
+          icon="add circle"
+          primary
+        />
+      </div>
+    );
   }
 }
 
