4f0e8d571295a137606f9360f4d16dba1710a9bd
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-15 20:34:35 +0200

    there were a typo. Sad.

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,15 +3,14 @@ import Web3 from 'web3';
 let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-  // We are in the borwser and metamask is running
-  web3 = new Web3(window.web3.currentPovider);
+  // We are in the browser and metamask is running.
+  web3 = new Web3(window.web3.currentProvider);
 } else {
-  // We are on the server OR the user is no running metamask
+  // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
     'https://rinkeby.infura.io/GN6SwkKdQHdRUH9fRO3Y'
   );
   web3 = new Web3(provider);
 }
-
 
 export default web3;
