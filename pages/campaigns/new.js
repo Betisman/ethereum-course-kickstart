281405340ba9e2ee3a86418870b631f0f7d019cb
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-15 23:58:36 +0200

    adding a router when creating a new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,6 +3,7 @@ import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
   state = {
@@ -22,6 +23,8 @@ class CampaignNew extends Component {
           // no need to specify gas because in the browser is metamask who calculate how much you can spend
           from: accounts[0]
         });
+
+      Router.pushRoute('/');
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
