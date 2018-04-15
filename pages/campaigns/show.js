21696cf6f6cba3b9e4698ffee423be909cb483a7
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-16 01:22:56 +0200

    adding a contribution form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -3,6 +3,7 @@ import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -57,6 +58,7 @@ class CampaignShow extends Component {
       <Layout>
         <h3>Campaign show</h3>
         {this.renderCards()}
+        <ContributeForm />
       </Layout>
     );
   }
